2af85fee82dea0151bc41336e1e72bbd9590e0b9
Shuji Ochi <shujiochi@ShujinoMacBook-Air.local>
2025-02-17 12:15:40 +0900

    feat: deploy.shを作成

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 
-# Todo
-# Write shell commands to create stack and set up server by ansible.
+# Set up script variables
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+STACK_NAME="db2-poc-stack"
+
+# Validate CloudFormation template
+if aws cloudformation validate-template --template-body file://"$SCRIPT_DIR/cfn/public.yml" > /dev/null 2>&1; then
+    echo "Validation of template: successful."
+    echo "Do you want to deploy the stack? [yes/no]"
+    read -r deploy_stack
+
+    if [[ "$deploy_stack" == "yes" ]]; then
+        aws cloudformation create-stack --stack-name "$STACK_NAME" --template-body file://"$SCRIPT_DIR/cfn/public.yml"
+        aws cloudformation wait stack-create-complete --stack-name "$STACK_NAME"
+    fi
+else
+    echo "Validation of template: failed."
+    exit 1
+fi
+
+# Get public IP of EC2 instance
+public_ip=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" | jq -r '.Stacks[0].Outputs[0].OutputValue?')
+
+if [[ -z "$public_ip" ]]; then
+    echo "Failed to retrieve public IP."
+    exit 1
+fi
+
+# Replace public_ip placeholder in Ansible configuration
+sed -i "s/#public_ip/$public_ip/" "$SCRIPT_DIR/ansible/group_vars/all.yml"
+
+# Run Ansible playbook
+cd "$SCRIPT_DIR/ansible" || exit
+
+if ansible-playbook -i inventory.ini site.yml --check; then
+    echo "Validation of playbook: successful."
+    echo "Do you want to set up a server? [yes/no]"
+    
+    read -r play_ansible
+    if [[ "$play_ansible" == "yes" ]]; then
+        ansible-playbook -i inventory.ini site.yml
+    fi
+else
+    echo "Validation of playbook: failed."
+    exit 1
+fi
